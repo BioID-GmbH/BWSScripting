a3b9bddd980e74b85fe54db8d1ad4ae3485ab829
mebealex <a.werner@bioid.com>
2024-10-11 15:38:57 +0200

    Update Restful-LivenessDetection.ps1

--- PowerShell/Restful-LivenessDetection.ps1
+++ PowerShell/Restful-LivenessDetection.ps1
@@ -1,1 +1,37 @@
+# Please go to the BWS Portal (bwsportal.bioid.com), select your client and click on the key symbol. 
+# At the end of the dialog you will find a link “Client Access Token (JWT)”. 
+# Click on it and generate a JWT and insert it here.
+$jwt = ''
+# To specify the endpoint, go to your client in the BWS Portal and select “Update client”. 
+# The grpc endpoint is specified there.
+$bwsEndpoint = '' # 'https://grpc.xxx-xxx.bioid.com'
+$fileName1 = 'C:\Users\testuser\1.png'
+$fileName2 = 'C:\Users\testuser\2.png'
 
+# Create empty array for liveImages
+$liveImages = @()
+# Create tag for head movement (relative to first image)
+$secondImageTags = @("")
+
+# Create first live image
+$liveImage1 = @{
+    "image" = [Convert]::ToBase64String([IO.File]::ReadAllBytes($fileName1))
+    "tags"  = @("") # empty array
+}
+# Add to liveImages array
+$liveImages += $liveImage1
+
+# Create second live image
+$liveImage2 = @{
+    "image" = [Convert]::ToBase64String([IO.File]::ReadAllBytes($fileName2))
+    "tags"  = $secondImageTags
+}
+# Add to liveImages array
+# $liveImages += $liveImage2
+
+
+# Fill out the request body and convert to json 
+$requestBody = @{ liveImages = $liveImages } | ConvertTo-Json -Depth 3
+
+$response = Invoke-WebRequest -Method POST -Headers @{"Content-Type"="application/json";"Authorization"="Bearer $jwt"} -Body $requestBody -Uri $bwsEndpoint"/api/v1/livenessdetection"
+Write-Output "Response: $response"
